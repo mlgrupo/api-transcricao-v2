9d147128674222c3669ecf749cfb1b061b4b45d7
Jardel <jardelkahne1@gmail.com>
2025-07-19 22:00:55 -0300

    Sem diarização

--- src/server.ts
+++ src/server.ts
@@ -33,8 +33,27 @@ expressApp.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-API-Key']
 }));
+
+// Adicionar headers CORS manualmente para garantir
+expressApp.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin && corsOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-API-Key');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 expressApp.use(express.json());
 expressApp.use(limiter);
 
